732ad1fe1881f9742c8752b80a6276dc5e1feec4
Bolter87 <workw6132@gmail.com>
2021-09-27 12:39:41 +0200

    init lib project

---
+++ lib-backend/src/main/java/com/librairiebenevole/libbackend/model/Section.java
@@ -0,0 +1,21 @@
+package com.librairiebenevole.libbackend.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Section {
+    private Long id;
+    private String name;
+    private String desc;
+    private List<String> booksList;
+    private int booksNb;
+
+}
---
+++ lib-backend/src/test/java/com/librairiebenevole/libbackend/LibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librairiebenevole.libbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
