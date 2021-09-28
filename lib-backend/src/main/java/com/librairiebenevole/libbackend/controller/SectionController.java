a4e6cdf68c51a234c075d8f169e0a9d2336a1e24
Bolter87 <workw6132@gmail.com>
2021-09-28 10:55:52 +0200

    add library database + debuging add request

--- lib-backend/src/main/java/com/librairiebenevole/libbackend/controller/SectionController.java
+++ lib-backend/src/main/java/com/librairiebenevole/libbackend/controller/SectionController.java
@@ -2,10 +2,12 @@ package com.librairiebenevole.libbackend.controller;
 
 import com.librairiebenevole.libbackend.model.Section;
 import com.librairiebenevole.libbackend.service.SectionService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +18,9 @@ public class SectionController {
     
 
     @PostMapping("/section")
-    public ResponseEntity<Section> addSection(Section section) {
+    public ResponseEntity<Section> addSection(@RequestBody Section section) {
+        System.out.println("section :" + section.toString());
         Section savedSection = sectionService.add(section);
-        return new ResponseEntity<Section>(savedSection, HttpStatus.CREATED);
+        return ResponseEntity.ok(savedSection);
     }
 }
--- lib-backend/src/main/java/com/librairiebenevole/libbackend/model/Book.java
+++ lib-backend/src/main/java/com/librairiebenevole/libbackend/model/Book.java
@@ -1,40 +1,36 @@
 package com.librairiebenevole.libbackend.model;
 
-import java.util.Set;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="SECTION")
-@ToString
-public class Section {
-
+@Table(name="BOOK")
+public class Book {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    
-    private String description;
+    private String title;
 
-    @OneToMany(targetEntity = com.librairiebenevole.libbackend.model.Book.class, cascade = CascadeType.ALL, mappedBy = "section")
-    private Set<Book> booksList;
-    
-    private int booksTotal;
+    @ManyToOne
+    @JoinColumn(name = "SECTION_ID", nullable = false)
+    private Section section;
+   
 }
